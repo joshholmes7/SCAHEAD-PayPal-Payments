c5b3446bb35e2bf79684e5bac0f9aef6324058ad
joshholmes7 <joshholmes7@yahoo.com>
2025-03-17 13:45:19 -0400

    Update getPaypalClientId.js

--- netlify/functions/getPaypalClientId.js
+++ netlify/functions/getPaypalClientId.js
@@ -1,14 +1,7 @@
 exports.handler = async function () {
     return {
         statusCode: 200,
-        headers: { "Content-Type": "application/json" }, // Ensure correct JSON format
-        body: JSON.stringify({ clientId: process.env.PAYPAL_CLIENT_ID }) 
-    };
-};
-exports.handler = async function () {
-    return {
-        statusCode: 200,
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ envVars: process.env }) // Debugging
+        body: JSON.stringify({ clientId: process.env.PAYPAL_CLIENT_ID }) 
     };
 };
